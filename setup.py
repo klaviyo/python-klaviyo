4477030418bc5d5614782cee63eb220e731eb6b9
Andrew Bialecki <andrew.bialecki@gmail.com>
2012-09-03 16:48:17 -0400

    Incrementing version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'klaviyo',
-    version = '0.9.1',
+    version = '0.9.2',
     packages = find_packages(),
 
     install_requires=[
