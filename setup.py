69893807ac3a8adb07a7bddfebda14740f56663f
Andrew Bialecki <andrew.bialecki@gmail.com>
2014-09-06 13:57:08 -0400

    Update request version, bump to 1.0.0.

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'klaviyo',
-    version = '0.9.3',
+    version = '1.0.0',
     packages = find_packages(),
 
     install_requires=[
-        'requests == 2.2.1',
+        'requests >= 2.2.1',
     ],
 
     # metadata for upload to PyPI
