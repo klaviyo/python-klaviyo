168c47d8609bfd128ba0aa7e1703da5c2709d0ab
Eduard James Aban <arceduardvincent@gmail.com>
2018-06-07 14:21:15 +0800

    Update python-klaviyo to be python3 compatible

--- klaviyo/__init__.py
+++ klaviyo/__init__.py
@@ -17,26 +17,29 @@ class KlaviyoException(Exception):
     pass
 
 class Klaviyo(object):
-    
+
     def __init__(self, api_token, api_server=KLAVIYO_API_SERVER):
         self.api_token = api_token
         self.api_server = api_server
-    
-    def track(self, event, email=None, id=None, properties=None, customer_properties=None,
+
+    def track(self, event, email=None, id_=None, properties=None, customer_properties=None,
         timestamp=None, ip_address=None, is_test=False):
-        
-        if email is None and id is None:
+
+        if email is None and id_ is None:
             raise KlaviyoException('You must identify a user by email or ID.')
-        
+
         if properties is None:
             properties = {}
-        
+
         if customer_properties is None:
             customer_properties = {}
-        
-        if email: customer_properties['email'] = email
-        if id: customer_properties['id'] = id
-        
+
+        if email:
+            customer_properties['email'] = email
+
+        if id_:
+            customer_properties['id'] = id_
+
         params = {
             'token' : self.api_token,
             'event' : event,
@@ -50,34 +53,37 @@ class Klaviyo(object):
 
         query_string = self._build_query_string(params, is_test)
         return self._request(KLAVIYO_TRACKING_ENDPOINT, query_string)
-    
-    def track_once(self, event, email=None, id=None, properties=None, customer_properties=None,
+
+    def track_once(self, event, email=None, id_=None, properties=None, customer_properties=None,
         timestamp=None, ip_address=None, is_test=False):
-        
+
         if properties is None:
             properties = {}
-        
+
         properties[TRACK_ONCE_KEY] = True
-        
-        return self.track(event, email=email, id=id, properties=properties, customer_properties=customer_properties,
+
+        return self.track(event, email=email, id_=id_, properties=properties, customer_properties=customer_properties,
             ip_address=ip_address, is_test=is_test)
-    
-    def identify(self, email=None, id=None, properties=None, is_test=False):
-        if email is None and id is None:
+
+    def identify(self, email=None, id_=None, properties=None, is_test=False):
+        if email is None and id_ is None:
             raise KlaviyoException('You must identify a user by email or ID.')
-        
+
         if properties is None:
             properties = {}
-        
-        if email: properties['email'] = email
-        if id: properties['id'] = id
-        
+
+        if email:
+            properties['email'] = email
+
+        if id_:
+            properties['id'] = id_
+
         query_string = self._build_query_string({
             'token' : self.api_token,
             'properties' : properties,
         }, is_test)
         return self._request(KLAVIYO_IDENTIFY_ENDPOINT, query_string)
-    
+
     def _normalize_timestamp(self, timestamp):
         if isinstance(timestamp, datetime.datetime):
             timestamp = time.mktime(timestamp.timetuple())
@@ -85,13 +91,13 @@ class Klaviyo(object):
         return timestamp
 
     def _build_query_string(self, params, is_test):
-        return urllib.urlencode({
-            KLAVIYO_DATA_VARIABLE : base64.b64encode(json.dumps(params)),
+        return urllib.parse.urlencode({
+            KLAVIYO_DATA_VARIABLE : base64.b64encode(json.dumps(params).encode('utf-8')),
             'test' : 1 if is_test else 0,
         })
 
     def _request(self, path, params):
         server = self.api_server
-        response = requests.get('http://%s/%s?%s' % (server, path, params))
-        
+        response = requests.get('https://%s/%s?%s' % (server, path, params))
+
         return response.text == '1'
