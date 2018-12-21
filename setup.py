3edf9b06bad4986df742572218af43b935a0bcf1
remstone7 <remstone7@gmail.com>
2018-12-21 10:17:18 -0500

    fix metric export

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'klaviyo',
-    version = '2.0.0',
+    version = '2.0.1',
     packages = find_packages(),
 
     install_requires=[
