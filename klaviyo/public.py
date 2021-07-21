bb4e2c43f6eb7cb61f259564c655cef0a8a54792
jon-batscha <jonathan.batscha@klaviyo.com>
2021-07-20 17:35:03 -0700

    need help debugging changes to TRACK, and will then copy over to IDENTIFY

--- klaviyo/public.py
+++ klaviyo/public.py
@@ -1,5 +1,9 @@
-from .api_helper import KlaviyoAPI
+from .api_helper import KlaviyoAPI, KlaviyoAPIResponse
 from .exceptions import KlaviyoException
+
+from json import dumps
+from requests import request
+from urllib.parse import urlencode
 
 class Public(KlaviyoAPI):
     # PUBLIC API PATHS
@@ -19,7 +23,8 @@ class Public(KlaviyoAPI):
         customer_properties=None,
         timestamp=None,
         ip_address=None,
-        is_test=False
+        is_test=False,
+        method='get'
         ):
         """Will create an event (metric) in Klaviyo.
 
@@ -34,10 +39,12 @@ class Public(KlaviyoAPI):
             timestamp (unix timestamp): Time the request is happening.
             ip_address (str): Ip address of the customer.
             is_test (bool): Should this be a test request.
+            method (str): 'post' or 'get'. Defaults to 'get'. We recommend 'post', but support 'get' for backwards compatibility.
 
         Returns:
             (str): 1 (pass) or 0 (fail).
         """
+
         self._valid_identifiers(email, external_id)
 
         if properties is None:
@@ -63,8 +70,29 @@ class Public(KlaviyoAPI):
         if ip_address:
             params['ip'] = ip_address
 
-        query_string = self._build_query_string(params, is_test)
-        return self._public_request(self.TRACK, query_string)
+        if method not in ['get','post']:
+
+            raise ValueError('method argument must be either "post" or "get"')
+
+        else:
+
+            if method == 'post':
+
+                url = f'{KlaviyoAPI.KLAVIYO_API_SERVER}/{self.TRACK}'
+
+                headers = {
+                    "Accept": "text/html",
+                    "Content-Type": "application/x-www-form-urlencoded"
+                }
+
+                response = request("POST", url, data={'data':urlencode(params)}, headers=headers)
+
+                return KlaviyoAPIResponse(response.status_code, response.json())
+
+            else: # original 'get' case
+
+                query_string = self._build_query_string(params, is_test)
+                return self._public_request(self.TRACK, query_string)
 
     def track_once(
         self, 
@@ -99,7 +127,7 @@ class Public(KlaviyoAPI):
         return self.track(event, email=email, external_id=external_id, properties=properties, customer_properties=customer_properties,
             timestamp=timestamp, ip_address=ip_address, is_test=is_test)
 
-    def identify(self, email=None, external_id=None, properties={}, is_test=False):
+    def identify(self, email=None, external_id=None, properties={}, is_test=False, method='get'):
         """Makes an identify call to Klaviyo API.
 
         This will create/update a user with its associated customer properties.
@@ -110,6 +138,8 @@ class Public(KlaviyoAPI):
             external_id (str or None): External id for customer.
             properties (dict): Information about the customer.
             is_test (bool): Should this be a test request.
+            method (str): 'post' or 'get'. Defaults to 'get'. We recommend 'post', but support 'get' for backwards compatibility.
+
         Returns:
             (str): 1 (pass) or 0 (fail).
         """
@@ -129,8 +159,11 @@ class Public(KlaviyoAPI):
             'properties': properties
         }
 
-        query_string = self._build_query_string(params, is_test)
-        return self._public_request(self.IDENTIFY, query_string)
+
+        # INSERT LOGIC FROM TRACK TO HERE
+
+            query_string = self._build_query_string(params, is_test)
+            return self._public_request(self.TRACK, query_string)
 
     @staticmethod
     def _valid_identifiers(email=None, external_id=None):
