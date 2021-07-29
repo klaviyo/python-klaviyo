aa26e6b4085d5de01a0ee86e6fe78a264c70ee98
Jonathan Batscha <61248025+jon-batscha@users.noreply.github.com>
2021-07-29 13:49:38 -0500

    Update klaviyo/api_helper.py
    Co-authored-by: Rem Stone <remstone7@gmail.com>

--- klaviyo/api_helper.py
+++ klaviyo/api_helper.py
@@ -175,7 +175,7 @@ class KlaviyoAPI(object):
         Raises:
             (KlaviyoConfigurationException): Information as to why the request won't work.
         """
-        if method not in [self.HTTP_GET, self.HTTP_POST]:
+        if method not in (self.HTTP_GET, self.HTTP_POST):
             raise KlaviyoConfigurationException("Invalid HTTP method for public request: must be 'get' of 'post'")
 
     def _v2_request(self, path, method, data={}):
